47bb33161f92fa7f741a21a04e02f4c175f07ed3
Ogua D. Leslie <ogua@ualberta.ca>
2024-08-29 15:54:25 -0600

    final

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,10 @@ const nextConfig = {
         domains: [
             "oaidalleapiprodscus.blob.core.windows.net"
         ]
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
     }
-};
 
+   }
 export default nextConfig;
